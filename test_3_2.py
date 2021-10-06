f12f082025329085da7b8e0d8defbb300015c116
Sergey Murzin <warka@1987@bk.ru>
2021-10-06 20:11:15 +0300

    fixture

--- test_3_2.py
+++ test_3_2.py
@@ -3,40 +3,26 @@ from selenium import webdriver
 import time
 
 
-def test_page(link):
+def test_page(links):
     try:
-        link = link
+        link = links
         browser = webdriver.Chrome()
         browser.get(link)
-
-        # Ваш код, который заполняет обязательные поля
         input1 = browser.find_element_by_css_selector('div .first_block .first')
         input1.send_keys("Ivan")
         input2 = browser.find_element_by_css_selector('div .first_block .second')
         input2.send_keys('Ivanov')
         input3 = browser.find_element_by_css_selector('div .first_block .third')
         input3.send_keys('1111')
-
-        # Отправляем заполненную форму
         button = browser.find_element_by_css_selector("button.btn")
         button.click()
-
-        # Проверяем, что смогли зарегистрироваться
-        # ждем загрузки страницы
         time.sleep(1)
-
-        # находим элемент, содержащий текст
         welcome_text_elt = browser.find_element_by_tag_name("h1")
-        # записываем в переменную welcome_text текст из элемента welcome_text_elt
         welcome_text = welcome_text_elt.text
-
-        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
         assert "Congratulations! You have successfully registered!" == welcome_text
 
     finally:
-        # ожидание чтобы визуально оценить результаты прохождения скрипта
-        time.sleep(3)
-        # закрываем браузер после всех манипуляций
+        time.sleep(1)
         browser.quit()
 
 
